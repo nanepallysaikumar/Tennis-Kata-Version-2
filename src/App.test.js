0ce7751fd7979ef678be5ca0acf5f5865e64e063
Sai Kumar <prasanna.kandregula.lpk@gmail.com>
2022-12-23 01:49:17 +0530

    Feat: When the running point of player one is 1 and the running point of player two is 0 then the running score should be "Fifteen - Love".

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,18 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { testConstants } from "./Constants/TestConstants";
 import App from "./App";
 
-const { GAME_SCORE, LOVE_ALL } = testConstants;
+const { GAME_SCORE, LOVE_ALL, PLAYER_ONE, FIFTEEN_LOVE } = testConstants;
 
 test(`When the individual points of both the players is "0", then the running score should be "Love-All".`, () => {
   render(<App />);
 
   expect(screen.getByTestId(GAME_SCORE).textContent).toEqual(LOVE_ALL);
+});
+
+test(`When the running point of player one is 1 and the running point of player two is 0 then the running score should be "Fifteen - Love".`, () => {
+  render(<App />);
+  fireEvent.click(screen.getByTestId(PLAYER_ONE));
+
+  expect(screen.getByTestId(GAME_SCORE).textContent).toEqual(FIFTEEN_LOVE);
 });
--- src/Utils/CalculateGameScore.js
+++ src/Utils/CalculateGameScore.js
@@ -1,11 +1,15 @@
 import { applicationConstants } from "../Constants/applicationConstants";
 import { isPlayersScoreEqualsToZero } from "../Validators/scoreValidator";
 
-const { LOVE_All } = applicationConstants;
+const { LOVE_All, FIFTEEN_LOVE, VALUE_ONE } = applicationConstants;
 
 const getGameScore = (playerOneScore, playerTwoScore) => {
   if (isPlayersScoreEqualsToZero(playerOneScore, playerTwoScore)) {
     return LOVE_All;
+  }
+
+  if (playerOneScore === VALUE_ONE) {
+    return FIFTEEN_LOVE;
   }
 };
 
